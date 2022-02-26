74531ff74a41ba2c982ab7ed7e2dbae32968a50d
kkoo <1451108685@qq.com>
2022-02-26 23:26:11 +0800

    "第一次学习"

--- SSM/MyBatis/learn_01/src/test/java/StudentTest.java
+++ SSM/MyBatis/learn_01/src/test/java/StudentTest.java
@@ -1,3 +1,15 @@
+import com.rzhylj.dao.StudentDAO;
+import com.rzhylj.entity.Student;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 /**
  * Copyright (C), 2019-2022, Kkoo
  * Author: kkoo
@@ -5,4 +17,43 @@
  * FileName: StudentTest
  */
 public class StudentTest {
+    @Test
+    public void searchStu() {
+        SqlSession sqlSession = null;
+        try {
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+            sqlSession = sqlSessionFactory.openSession();
+            StudentDAO userDao = sqlSession.getMapper(StudentDAO.class);
+            System.out.println("通过查询学生的id 获取学生的信息  返回值 Student");
+            Student student = userDao.searchStu(1001);
+            System.out.println(student);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
+
+    @Test
+    public void searchCls() {
+        SqlSession sqlSession = null;
+        try {
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+            sqlSession = sqlSessionFactory.openSession();
+            StudentDAO userDao = sqlSession.getMapper(StudentDAO.class);
+            System.out.println("通过班级查询所有学生  返回值 list");
+            List<Student> student = userDao.searchCls(1);
+            System.out.println(student);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
 }
